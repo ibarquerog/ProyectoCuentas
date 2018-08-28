e2f2f5769b56df73f6da543cefa5a1aacdb87e83
ibarquerog <nacho.bg17@gmail.com>
2018-08-27 19:23:03 -0600

    Commit inicial

---
+++ ProyectoCuentas/ProyectoCuentas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoCuentas.Startup))]
+namespace ProyectoCuentas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
